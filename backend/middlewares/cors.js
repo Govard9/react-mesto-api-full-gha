ebb89e864cb89516da240c1e79d45bba749d637c
govard9 <web-supsu@yandex.ru>
2023-06-02 16:54:06 +0700

    feat: Настройка CORS.

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -16,6 +16,8 @@ module.exports = (req, res, next) => {
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+
+    return res.end();
   }
 
   if (method === 'OPTIONS') {
