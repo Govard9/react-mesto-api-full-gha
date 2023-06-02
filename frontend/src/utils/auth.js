fbd79eaa0c7513423ac4c757cf7f8c92ea8b1726
govard9 <web-supsu@yandex.ru>
2023-06-02 13:08:57 +0700

    feat: тест запросов.

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -67,7 +67,7 @@ export class Auth extends Api {
 }
 
 const auth = new Auth(
-  'https://auth.nomoreparties.co',
+  'http://api.mesto.govard.nomoredomains.rocks/',
   '2ba4031f-f997-482b-b349-7c66bdec4853'
 );
 
