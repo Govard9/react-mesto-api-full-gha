ba60464a4916c6f9a809aa45b91517285509d6c4
govard9 <web-supsu@yandex.ru>
2023-06-03 12:10:43 +0700

    feat: Настройка CORS.

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -7,25 +7,8 @@ const allowedCors = [
   'http://localhost:3000',
 ];
 
-const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
-
-module.exports = (req, res, next) => {
-  const { method } = req;
-  const { origin } = req.headers;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-
-    return res.end();
-  }
-
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-  }
-
-  return next();
-};
+export const corsOptions = {
+  origin: allowedCors,
+  optionsSuccesStatus: 200,
+  credentials: true
+}
