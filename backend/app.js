f3623b2676e3d9f60366753224bf40a4761e3ef0
govard9 <web-supsu@yandex.ru>
2023-06-06 11:03:47 +0700

    feat: краштест сервера для проверки pm2.

--- backend/app.js
+++ backend/app.js
@@ -33,12 +33,12 @@ app.use(helmet());
 
 app.use(express.json());
 app.use(requestLogger); // логгер запросов
-app.use(cors(corsOptions));
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
+app.use(cors(corsOptions));
 app.use(router);
 app.use(errorLogger); // логгер ошибок
 
