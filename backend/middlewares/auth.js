d7d4fa7cb9472c06bb14e807ee15e78c06635790
govard9 <web-supsu@yandex.ru>
2023-06-06 11:39:30 +0700

    feat: прогнать код через eslint.

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const ErrorAuthorization = require('../errors/error-authorization');
+
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
