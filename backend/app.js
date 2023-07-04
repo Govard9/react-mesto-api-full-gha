f0cd7bac74e0980a46fb503ef473c9afb8544090
govard9 <web-supsu@yandex.ru>
2023-07-04 12:53:23 +0700

    fix: Вернуть всё на место.

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,23 @@ require('dotenv').config();
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const allowedCors = [
+  'http://api.mesto.govard.nomoredomains.rocks',
+  'https://api.mesto.govard.nomoredomains.rocks',
+  'https://mesto.govard.nomoredomains.rocks',
+  'http://mesto.govard.nomoredomains.rocks',
+  'localhost:3000',
+  'http://localhost',
+  'http://localhost:3001',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin: allowedCors,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
 const app = express();
 
 const handleErrors = require('./middlewares/handleErrors');
@@ -15,7 +32,7 @@ app.use(helmet());
 
 app.use(express.json());
 app.use(requestLogger); // логгер запросов
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(router);
 app.use(errorLogger); // логгер ошибок
 
