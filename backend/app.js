a7eed19e706cea0c62fb935e6885c8deea095041
govard9 <web-supsu@yandex.ru>
2023-07-04 12:49:58 +0700

    fix: Убрать CORS настройки для проверки доступа с любого адреса.

--- backend/app.js
+++ backend/app.js
@@ -7,23 +7,6 @@ require('dotenv').config();
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const allowedCors = [
-  'http://api.mesto.govard.nomoredomains.rocks',
-  'https://api.mesto.govard.nomoredomains.rocks',
-  'https://mesto.govard.nomoredomains.rocks',
-  'http://mesto.govard.nomoredomains.rocks',
-  'localhost:3000',
-  'http://localhost',
-  'http://localhost:3001',
-  'http://localhost:3000',
-];
-
-const corsOptions = {
-  origin: allowedCors,
-  optionsSuccessStatus: 200,
-  credentials: true,
-};
-
 const app = express();
 
 const handleErrors = require('./middlewares/handleErrors');
@@ -32,7 +15,7 @@ app.use(helmet());
 
 app.use(express.json());
 app.use(requestLogger); // логгер запросов
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(router);
 app.use(errorLogger); // логгер ошибок
 
@@ -49,4 +32,4 @@ app.use(errors()); // обработчик ошибок celebrate
 
 app.use(handleErrors);
 
-app.listen(3000, () => { console.log('Server started.'); });
+app.listen(3001, () => { console.log('Server started.'); });
